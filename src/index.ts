119d8fda2d107153ef5dadabd7f07e2f1c7152fb
DasIschBims <dasischbims@gmail.com>
2022-10-30 18:19:52 +0100

    stopped side panel from auto-opening

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,6 @@ const particleYVelocityElement = document.getElementById('velocity-y') as HTMLIn
 
 
 window.onload = () => {
-  settingsToggle.checked = true;
   colorPickerElement.defaultValue = settings.color;
 }
 
