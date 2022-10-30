fdf32ea31f814ad335043d643925950fe1704325
Snowpack Build <actions@github.com>
2022-10-30 22:11:44 +0000

    Commit from GitHub Actions (Build and Deploy)

--- docs/dist/index.js
+++ docs/dist/index.js
@@ -10,8 +10,14 @@ const particleMinSizeElement = document.getElementById("min-size");
 const particleMaxSizeElement = document.getElementById("max-size");
 const particleXVelocityElement = document.getElementById("velocity-x");
 const particleYVelocityElement = document.getElementById("velocity-y");
+const particleShapeCircle = document.getElementById("circle-input");
+const particleShapeSquare = document.getElementById("square-input");
+const particleShapeTriangle = document.getElementById("triangle-input");
+const particleShapeStar = document.getElementById("star-input");
+const generalClearCanvas = document.getElementById("clear-toggle");
 window.onload = () => {
-  colorPickerElement.defaultValue = settings.color;
+  colorPickerElement.defaultValue = "#ffffff";
+  settingsToggle.checked = true;
 };
 const settings = {
   color: "rainbow",
@@ -22,8 +28,26 @@ const settings = {
   velocity: {
     x: 2,
     y: 2
+  },
+  shape: "circle",
+  general: {
+    clearCanvas: true
   }
 };
+switch (settings.shape) {
+  case "circle":
+    particleShapeCircle.checked = true;
+    break;
+  case "square":
+    particleShapeSquare.checked = true;
+    break;
+  case "triangle":
+    particleShapeTriangle.checked = true;
+    break;
+  case "star":
+    particleShapeStar.checked = true;
+    break;
+}
 function updateSettings() {
   if (rainbowColorInputElement.checked) {
     settings.color = "rainbow";
@@ -34,6 +58,19 @@ function updateSettings() {
   settings.size.max = parseInt(particleMaxSizeElement.value);
   settings.velocity.x = parseInt(particleXVelocityElement.value);
   settings.velocity.y = parseInt(particleYVelocityElement.value);
+  if (particleShapeCircle.checked) {
+    settings.shape = "circle";
+  }
+  if (particleShapeSquare.checked) {
+    settings.shape = "square";
+  }
+  if (particleShapeTriangle.checked) {
+    settings.shape = "triangle";
+  }
+  if (particleShapeStar.checked) {
+    settings.shape = "star";
+  }
+  settings.general.clearCanvas = generalClearCanvas.checked;
 }
 function resizeCanvas() {
   canvas.width = window.innerWidth;
@@ -48,17 +85,17 @@ const mouse = {
 canvas.addEventListener("mousemove", (e) => {
   mouse.x = e.clientX;
   mouse.y = e.clientY;
-  particleArray.push(new Particle({x: mouse.x, y: mouse.y}, settings.color, {min: settings.size.min, max: settings.size.max}, {velocityX: settings.velocity.x, velocityY: settings.velocity.y}));
+  particleArray.push(new Particle({x: mouse.x, y: mouse.y}, settings.color, {min: settings.size.min, max: settings.size.max}, {velocityX: settings.velocity.x, velocityY: settings.velocity.y}, settings.shape));
 });
 canvas.addEventListener("click", () => {
   for (let i = 0; i < 10; i++) {
-    particleArray.push(new Particle({x: mouse.x, y: mouse.y}, settings.color, {min: settings.size.min, max: settings.size.max}, {velocityX: settings.velocity.x, velocityY: settings.velocity.y}));
+    particleArray.push(new Particle({x: mouse.x, y: mouse.y}, settings.color, {min: settings.size.min, max: settings.size.max}, {velocityX: settings.velocity.x, velocityY: settings.velocity.y}, settings.shape));
   }
 });
 window.addEventListener("keydown", (e) => {
   if (e.code === "Space") {
     for (let i = 0; i < 100; i++) {
-      particleArray.push(new Particle({x: Math.random() * canvas.width, y: Math.random() * canvas.height}, settings.color, {min: settings.size.min, max: settings.size.max}, {velocityX: settings.velocity.x, velocityY: settings.velocity.y}));
+      particleArray.push(new Particle({x: Math.random() * canvas.width, y: Math.random() * canvas.height}, settings.color, {min: settings.size.min, max: settings.size.max}, {velocityX: settings.velocity.x, velocityY: settings.velocity.y}, settings.shape));
     }
   }
   if (e.code === "Digit1") {
@@ -66,20 +103,23 @@ window.addEventListener("keydown", (e) => {
   }
   if (e.code === "Digit2") {
     particleArray.splice(0, particleArray.length);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     console.log("%cCleared particles", "color: cyan");
   }
 });
 class Particle {
-  constructor(position, color, sizeObj, velocity) {
+  constructor(position, color, sizeObj, velocity, shape) {
     this.position = position;
     this.color = color;
     this.sizeObj = sizeObj;
     this.velocity = velocity;
+    this.shape = shape;
     this.position.x = position.x;
     this.position.y = position.y;
     this.color = color;
     this.size = Math.floor(Math.random() * (sizeObj.max - sizeObj.min) + sizeObj.min);
     this.velocity = {velocityX: Math.random() * this.velocity.velocityX - this.velocity.velocityX / 2, velocityY: Math.random() * this.velocity.velocityY - this.velocity.velocityY / 2};
+    this.shape = shape;
   }
   update() {
     this.position.x += this.velocity.velocityX;
@@ -93,9 +133,39 @@ class Particle {
     } else {
       ctx.fillStyle = this.color;
     }
-    ctx.beginPath();
-    ctx.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 2);
-    ctx.fill();
+    switch (this.shape) {
+      case "circle":
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+        break;
+      case "square":
+        ctx.fillRect(this.position.x, this.position.y, this.size * 1.5, this.size * 1.5);
+        break;
+      case "triangle":
+        ctx.beginPath();
+        ctx.moveTo(this.position.x, this.position.y - this.size);
+        ctx.lineTo(this.position.x + this.size, this.position.y + this.size);
+        ctx.lineTo(this.position.x - this.size, this.position.y + this.size);
+        ctx.fill();
+        break;
+      case "star":
+        let alpha = 2 * Math.PI / 10;
+        ctx.beginPath();
+        for (var i = 11; i != 0; i--) {
+          var r = this.size * (i % 2 + 1) / 1.5;
+          var omega = alpha * i;
+          ctx.lineTo(r * Math.sin(omega) + this.position.x, r * Math.cos(omega) + this.position.y);
+        }
+        ctx.closePath();
+        ctx.fill();
+        break;
+      default:
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+        break;
+    }
   }
 }
 function handleParticles() {
@@ -109,7 +179,9 @@ function handleParticles() {
   }
 }
 function update() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (settings.general.clearCanvas) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
   handleParticles();
   hue += 0.25;
   requestAnimationFrame(update);
